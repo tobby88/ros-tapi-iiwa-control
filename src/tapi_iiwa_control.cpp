fc9a45a806b94858f06c5692f351bb31ef3d0365
tobby <tobby@tobby.eu>
2016-09-09 00:29:52 +0200

    Add subscribers and their callbacks

--- src/tapi_iiwa_control.cpp
+++ src/tapi_iiwa_control.cpp
@@ -5,18 +5,82 @@
 
 namespace Tapi
 {
+// Constructor/Destructor
 iiwaControl::iiwaControl(ros::NodeHandle* nh) : nh(nh)
 {
+  std::fill(gotValues, gotValues + sizeof(gotValues), false);
   tclient = new Tapi::ServiceClient(nh, "iiwa Controller");
   tpub = new Tapi::Publisher(nh, "iiwa Controller");
   tsub = new Tapi::Subscriber(nh, "iiwa Controller");
   iiwaModeClient = tclient->AddFeature<tapi_iiwa::OpenIGTLStateService>("Mode");
   iiwaPub = tpub->AddFeature<geometry_msgs::PoseStamped>("Goal Pose", 1);
+  coefficients[0] =
+      tsub->AddFeature(SubscribeOptionsForTapi(std_msgs::Float64, 1, &iiwaControl::gotAngularX), "Angular X");
+  coefficients[1] =
+      tsub->AddFeature(SubscribeOptionsForTapi(std_msgs::Float64, 1, &iiwaControl::gotAngularY), "Angular Y");
+  coefficients[2] =
+      tsub->AddFeature(SubscribeOptionsForTapi(std_msgs::Float64, 1, &iiwaControl::gotAngularZ), "Angular Z");
+  coefficients[3] =
+      tsub->AddFeature(SubscribeOptionsForTapi(std_msgs::Float64, 1, &iiwaControl::gotLinearX), "Linear X");
+  coefficients[4] =
+      tsub->AddFeature(SubscribeOptionsForTapi(std_msgs::Float64, 1, &iiwaControl::gotLinearY), "Linear Y");
+  coefficients[5] =
+      tsub->AddFeature(SubscribeOptionsForTapi(std_msgs::Float64, 1, &iiwaControl::gotLinearZ), "Linear Z");
+  tsub->AddFeature(SubscribeOptionsForTapi(sensor_msgs::Joy, 1, &iiwaControl::gotJoy), "[Optional] Full Joy Message");
 }
+
 iiwaControl::~iiwaControl()
 {
   delete tclient;
   delete tpub;
   delete tsub;
+}
+
+// Private member functions
+void iiwaControl::gotAngularX(const std_msgs::Float64::ConstPtr& msg)
+{
+  angular[0] = msg->data * *coefficients[0];
+  gotValues[0] = true;
+}
+
+void iiwaControl::gotAngularY(const std_msgs::Float64::ConstPtr& msg)
+{
+  angular[1] = msg->data * *coefficients[1];
+  gotValues[1] = true;
+}
+
+void iiwaControl::gotAngularZ(const std_msgs::Float64::ConstPtr& msg)
+{
+  angular[2] = msg->data * *coefficients[2];
+  gotValues[2] = true;
+}
+
+void iiwaControl::gotJoy(const sensor_msgs::Joy::ConstPtr& msg)
+{
+  angular[0] = msg->axes[0];
+  angular[1] = msg->axes[1];
+  angular[2] = msg->axes[2];
+  linear[0] = msg->axes[3];
+  linear[1] = msg->axes[4];
+  linear[2] = msg->axes[5];
+  std::fill(gotValues, gotValues + sizeof(gotValues), true);
+}
+
+void iiwaControl::gotLinearX(const std_msgs::Float64::ConstPtr& msg)
+{
+  linear[0] = msg->data * *coefficients[3];
+  gotValues[3] = true;
+}
+
+void iiwaControl::gotLinearY(const std_msgs::Float64::ConstPtr& msg)
+{
+  linear[1] = msg->data * *coefficients[4];
+  gotValues[4] = true;
+}
+
+void iiwaControl::gotLinearZ(const std_msgs::Float64::ConstPtr& msg)
+{
+  linear[2] = msg->data * *coefficients[5];
+  gotValues[5] = true;
 }
 }
--- src/tapi_iiwa_control.hpp
+++ src/tapi_iiwa_control.hpp
@@ -5,6 +5,9 @@
 #include "ros/publisher.h"
 #include "ros/service_client.h"
 #include "ros/subscriber.h"
+#include "sensor_msgs/Joy.h"
+#include "std_msgs/Float64.h"
+#include "std_msgs/Header.h"
 #include "tapi_lib/tapi_lib.hpp"
 
 namespace Tapi
@@ -13,17 +16,31 @@ class iiwaControl
 {
 public:
   // Constructor/Desctructor
-  iiwaControl(ros::NodeHandle *nh);
+  iiwaControl(ros::NodeHandle* nh);
   ~iiwaControl();
 
 private:
+  // Private member functions
+  void gotAngularX(const std_msgs::Float64::ConstPtr& msg);
+  void gotAngularY(const std_msgs::Float64::ConstPtr& msg);
+  void gotAngularZ(const std_msgs::Float64::ConstPtr& msg);
+  void gotJoy(const sensor_msgs::Joy::ConstPtr& msg);
+  void gotLinearX(const std_msgs::Float64::ConstPtr& msg);
+  void gotLinearY(const std_msgs::Float64::ConstPtr& msg);
+  void gotLinearZ(const std_msgs::Float64::ConstPtr& msg);
+
   // Private member variables
-  ros::ServiceClient **iiwaModeClient;
-  ros::Publisher *iiwaPub;
-  ros::NodeHandle *nh;
-  Tapi::ServiceClient *tclient;
-  Tapi::Publisher *tpub;
-  Tapi::Subscriber *tsub;
+  double angular[3];
+  double* coefficients[6];
+  bool gotValues[6];
+  std_msgs::Header header;
+  ros::ServiceClient** iiwaModeClient;
+  ros::Publisher* iiwaPub;
+  double linear[3];
+  ros::NodeHandle* nh;
+  Tapi::ServiceClient* tclient;
+  Tapi::Publisher* tpub;
+  Tapi::Subscriber* tsub;
 };
 }
 
