ed11b3569b082ceb717b0b5a3f500b929fa7add0
tobby <tobby@tobby.eu>
2016-09-10 01:01:53 +0200

    Add copyright

--- src/tapi_iiwa_control.hpp
+++ src/tapi_iiwa_control.hpp
@@ -1,4 +1,6 @@
 /******************************************************************************
+*  Copyright (C) 2016 by Tobias Holst                                         *
+*                                                                             *
 *  This file is part of tapi_iiwa_control.                                    *
 *                                                                             *
 *  tapi_iiwa_control is free software: you can redistribute it and/or modify  *
